0a06ec691773a336c48540cb7ac7cd11c91c785b
VasquezDav <davasquez7@miners.utep.edu>
2024-12-16 13:32:06 -0700

    added login sevice

---
+++ frontend/src/app/login/login.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent {
+  username = '';
+  password = '';
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  login(): void {
+    this.authService.login(this.username, this.password).subscribe({
+      next: () => {
+        console.log('Login successful!');
+        // this.router.navigate(['/ to home page']);
+      },
+      error: (err) => {
+        console.error('Login failed:', err);
+        alert('Invalid username or password');
+      },
+    });
+  }
+}
